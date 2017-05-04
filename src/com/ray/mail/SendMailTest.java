15acc980f08303911cbc66e3862cce40e6600789
persadewh <persadewh@126.com>
2017-05-04 10:09:12 +0800

    新增代码包 测试java的邮件发送功能

---
+++ src/com/ray/mail/SendMailTest.java
@@ -0,0 +1,17 @@
+package com.ray.mail;
+
+/**
+ * @author : ray
+ * @date : 2017年5月4日
+ * 使用javamail测试简单的邮件发送功能
+ */
+public class SendMailTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+	}
+
+}
