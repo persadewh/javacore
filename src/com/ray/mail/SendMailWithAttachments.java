e3cd818adc142bbc0a8ffc2102ba4397added2e5
persadewh <persadewh@126.com>
2017-05-04 10:47:44 +0800

    修正格式

--- src/com/ray/mail/SendMailWithAttachments.java
+++ src/com/ray/mail/SendMailWithAttachments.java
@@ -32,18 +32,18 @@ public static final String MAILSERVER = "*********";
 
 		// 1. 创建参数配置, 用于连接邮件服务器的参数配置
 		Properties props = new Properties();                    // 参数配置
-        props.setProperty("mail.transport.protocol", "smtp");   // 使用的协议（JavaMail规范要求）
-        props.setProperty("mail.smtp.host", MAILSERVER);   // 发件人的邮箱的 SMTP 服务器地址
-        props.setProperty("mail.smtp.auth", "true");            // 需要请求认证
-        
-        // 2. 根据配置创建会话对象, 用于和邮件服务器交互
-        Session session = Session.getDefaultInstance(props);
-        session.setDebug(true);                                 // 设置为debug模式, 可以查看详细的发送 log
+		props.setProperty("mail.transport.protocol", "smtp");   // 使用的协议（JavaMail规范要求）
+		props.setProperty("mail.smtp.host", MAILSERVER);   // 发件人的邮箱的 SMTP 服务器地址
+		props.setProperty("mail.smtp.auth", "true");            // 需要请求认证
 		
-        // 3. 创建一封邮件
-        MimeMessage message = new MimeMessage(session);
-        try 
-        {
+		// 2. 根据配置创建会话对象, 用于和邮件服务器交互
+		Session session = Session.getDefaultInstance(props);
+		session.setDebug(true);                                 // 设置为debug模式, 可以查看详细的发送 log
+		
+		// 3. 创建一封邮件
+		MimeMessage message = new MimeMessage(session);
+		try 
+		{
 			message.setFrom(new InternetAddress(USER, "ray", "UTF-8"));
 			message.setRecipient(RecipientType.TO, new InternetAddress(USER, "ray", "UTF-8"));
 			message.setSubject("测试邮件", "UTF-8");
@@ -64,10 +64,10 @@ public static final String MAILSERVER = "*********";
 			message.saveChanges();
 			
 			// 4. 根据 Session 获取邮件传输对象
-	        Transport transport = session.getTransport();
-	        transport.connect(USER, PWD);
-	        transport.sendMessage(message, message.getAllRecipients());
-	        transport.close();
+			Transport transport = session.getTransport();
+			transport.connect(USER, PWD);
+			transport.sendMessage(message, message.getAllRecipients());
+			transport.close();
 		} catch (UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
